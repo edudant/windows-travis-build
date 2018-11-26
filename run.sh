f3f8386eb5855c005dc89cc257aa528667be0d37
Jiri Bubnik <jiri.bubnik@centrum.cz>
2018-11-26 15:34:25 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,15 @@
 export PATH=/cygdrive/c/tools/cygwin/bin:$PATH
-make -v
+
+url=$(curl -silent https://api.adoptopenjdk.net/openjdk10/nightly/x64_Win/ | grep 'binary_link' | grep -Eo '(http|https)://[^"]+' | head -1)
+wget --quiet ${url}
+zip_file=$(basename ${url})
+target=$(unzip -Z1 ${zip_file} | grep 'bin/javac'  | tr '/' '\n' | tail -3 | head -1)
+unzip -q ${zip_file}
+
+export JAVA_HOME=$(cd "${target}"; pwd)
+export PATH=${JAVA_HOME}/bin:$PATH 
+
+cd openjdk-build
+export LOG=info
+./makejdk-any-platform.sh jdk11u
+
